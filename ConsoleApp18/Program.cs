fa40db19b5b4a96457d5d5cf200d98cdebe93cb1
Peter <sheraium@gmail.com>
2020-07-14 08:35:25 +0800

    18. Observable.Create

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Timers;
+using Rx.Extensions;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var obs = Observable.Create<string>(observer =>
+            {
+                var timer = new Timer(1000);
+                timer.Elapsed += (sender, eventArgs) => observer.OnNext($"tick {eventArgs.SignalTime}");
+                timer.Elapsed += TimerOnElapsed;
+                timer.Start();
+                return () =>
+                {
+                    timer.Elapsed -= TimerOnElapsed;
+                    timer.Dispose();
+                };
+            });
+
+            var sub = obs.Inspect("timer");
+            Console.ReadLine();
+
+            sub.Dispose();
+            Console.ReadLine();
+        }
+
+        private static void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine($"tock {e.SignalTime}");
+        }
+    }
+}
---
+++ Rx.Extensions/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rx.Extensions
+{
+    public static class ExtensionMethods
+    {
+        public static IDisposable Inspect<T>(this IObservable<T> self, string name)
+        {
+            return self.Subscribe(
+                x => Console.WriteLine($"{name} has generated value {x}"),
+                ex => Console.WriteLine($"{name} has generated exception {ex.Message}"),
+                () => Console.WriteLine($"{name} has completed"));
+        }
+    }
+}

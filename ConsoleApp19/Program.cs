4fe6f5c9803a41e33db1e9cc90ee8b1576a6a584
Peter <sheraium@gmail.com>
2020-07-14 09:14:50 +0800

    19. Sequence Generators

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reactive.Linq;
+using Rx.Extensions;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var tenToTwenty = Observable.Range(10, 11);
+            tenToTwenty.Inspect("range");
+
+            var generated = Observable.Generate(
+                 1,
+                 value => value < 100,
+                 value => value * value + 1,
+                 value => $"[{value}]"); // select()
+            generated.Inspect("generate");
+
+            var interval = Observable.Interval(TimeSpan.FromMilliseconds(500));
+            using (interval.Inspect("interval"))
+            {
+                Console.ReadKey();
+            }
+
+            var timer = Observable.Timer(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+            timer.Inspect("timer");
+            Console.ReadLine();
+        }
+    }
+}

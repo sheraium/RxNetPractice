00a87a63c1a890c0757220498d8ef5554a0c85ed
Peter <sheraium@gmail.com>
2021-03-21 18:36:18 +0800

    add ConsoleAppIntervalAsync

---
+++ ConsoleAppIntervalAsync/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleAppIntervalAsync
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Observable
+                .Interval(TimeSpan.FromMilliseconds(1))
+                .Select(l => Observable.FromAsync(ProcAsync))
+                .Concat()
+                .Subscribe();
+
+            Console.ReadLine();
+        }
+
+        private static async Task ProcAsync()
+        {
+            Console.WriteLine($"Interval Start: {DateTime.Now:hh:mm:ss.fff}");
+            Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(1000);
+            Console.WriteLine($"Interval End: {DateTime.Now:hh:mm:ss.fff}");
+        }
+    }
+}
